3ea49e08bced3ba0215a9eb9accec11732a2b809
winniemonica <winniemonica@users.noreply.github.com>
2022-03-12 09:00:49 +0000

    Deploying to gh-pages from @ rg-km/se-be4-kelompok1@c87dcdaa97eea3ea9990bb8696b249ef1c771b96 🚀

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const CANVAS_SIZE = 400
 const DEFAULTLIFE = 3;
 const REDRAW_INTERVAL = 50;
 let DEFAULTSPEED = 150;
-const DEFAULTCOLORBARRIER = "black";
-const WIDTH = CANVAS_SIZE / CELL_SIZE;
-const HEIGHT = CANVAS_SIZE / CELL_SIZE;
 const LEVELS = [
     { level: 1, speed: DEFAULTSPEED, },
     { level: 2, speed: 100, },
@@ -13,53 +10,6 @@ const LEVELS = [
     { level: 4, speed: 60, },
     { level: 5, speed: 50, },
 ];
-const OBSTACLES = [
-    {
-        level: 1,
-        obstacle: [
-            //obstacle code here
-        ]
-    },
-    {
-        level: 2,
-        obstacle: [
-            {
-                position: initBarrier(50, 100, 5, 150),
-                color: DEFAULTCOLORBARRIER
-            },
-            {
-                position: initBarrier(350, 100, 5, 150),
-                color: DEFAULTCOLORBARRIER,
-            },
-        ]
-    },
-    {
-        level: 3,
-        obstacle: [
-            {
-                position: initBarrier(50, 100, 250, 5),
-                color: DEFAULTCOLORBARRIER,
-            },
-            {
-                position: initBarrier(50, 250, 250, 5),
-                color: DEFAULTCOLORBARRIER,
-            }
-        ]
-    },
-    {
-        level: 4,
-        obstacle: [
-            //obstacle code here
-        ]
-    },
-    {
-        level: 5,
-        obstacle: [
-            //obstacle code here
-        ]
-    },
-];
-
 let snake1 = initSnake()
 let apple = {
     type: "food",
@@ -95,17 +45,6 @@ function initSnake() {
         life: DEFAULTLIFE,
     }
 }
-
-function recentSnake(snake) {
-    return {
-        ...initHeadAndBody(),
-        direction: initDirection(),
-        score: snake.score,
-        life: snake.life,
-        level: snake.level,
-    }
-}
-
 function initHeadAndBody() {
     let head = initPosition()
     let body = [{ x: head.x, y: head.y }]
@@ -124,15 +63,6 @@ function initPosition() {
 
 function initDirection() {
     return Math.floor(Math.random() * 4)
-}
-
-function initBarrier(x, y, width, height) {
-    return {
-        x: x,
-        y: y,
-        width: width,
-        height: height
-    }
 }
 
 function showIcon(ctx, path, x, y, width = 10, height = 10) {
@@ -175,11 +105,6 @@ function drawCell(ctx, x, y, img = null) {
     }
 }
 
-function drawObstacle(ctx, x, y, width, height, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height);
-}
-
 function drawScore(snake, canvas) {
     let scoreCanvas = document.getElementById(canvas)
     if (scoreCanvas !== null) {
@@ -196,11 +121,8 @@ function draw() {
     setInterval(function () {
         let snakeCanvas = document.getElementById("snakeBoard");
         let ctx = snakeCanvas.getContext("2d");
-        let img = document.getElementById("apple");
 
         ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-        showObstacle(snake1);
-
 
         drawCell(ctx, snake1.head.x, snake1.head.y, "snake")
         for (let i = 1; i < snake1.body.length; i++) {
@@ -220,12 +142,12 @@ function draw() {
 draw()
 
 
+
 function eat(snake, feed) {
-    let eat = new Audio()
-    eat.src = "./assets/eat.mp3"
     if (snake.head.x === feed.position.x && snake.head.y === feed.position.y) {
         eat.play()
         feed.position = initPosition()
+
         snake.score++
         if (feed.type == "food") {
             snake.body.push({ x: snake.head.x, y: snake.head.y });
@@ -281,59 +203,20 @@ function drawSpeed(snake, canvas) {
 
 }
 
-function showObstacle(snake) {
-    let snakeCanvas = document.getElementById("snakeBoard");
-    let ctx = snakeCanvas.getContext("2d");
-    for (let i = 0; i < OBSTACLES.length; i++) {
-        for (let j = 0; j < OBSTACLES[i].obstacle.length; j++) {
-            if (snake.level == OBSTACLES[i].level) {
-                if (OBSTACLES[i].obstacle.length > 0) {
-                    ctx.fillStyle = OBSTACLES[i].obstacle[j].color;
-                    drawObstacle(ctx, OBSTACLES[i].obstacle[j].position.x, OBSTACLES[i].obstacle[j].position.y, OBSTACLES[i].obstacle[j].position.width, OBSTACLES[i].obstacle[j].position.height, OBSTACLES[i].obstacle[j].color);
-                }
-            }
-        }
-    }
-}
-
 function checkCollision(snakes) {
     let isCollide = false
-    let gameOver = new Audio()
-    gameOver.src = "./assets/GameOver.mp3"
     for (let i = 0; i < snakes.length; i++) {
-        for (let j = 0; j < snakes.length; j++) {
-            for (let k = 1; k < snakes[j].body.length; k++) {
-                if (snakes[i].head.x === snakes[j].body[k].x && snakes[i].head.y === snakes[j].body[k].y) {
+            for (let k = 1; k < snakes[0].body.length; k++) {
+                if (snakes[i].head.x === snakes[i].body[k].x && snakes[i].head.y === snakes[i].body[k].y) {
                     isCollide = true
-                }
             }
         }
     }
-
-    //tembok
-    for (let p = 0; p < snakes.length; p++) {
-        for (let i = 0; i < OBSTACLES.length; i++) {
-            for (let j = 0; j < OBSTACLES[i].obstacle.length; j++) {
-                if (snakes[p].level == OBSTACLES[i].level && OBSTACLES[i].obstacle.length > 0) {
-                    if (snakes[p].head.x >= (Math.floor(OBSTACLES[i].obstacle[j].position.x / CELL_SIZE)) && snakes[p].head.y >= (Math.floor(OBSTACLES[i].obstacle[j].position.y / CELL_SIZE))
-                        && snakes[p].head.y <= (Math.floor(OBSTACLES[i].obstacle[j].position.height / HEIGHT)) + Math.floor(OBSTACLES[i].obstacle[j].position.y / CELL_SIZE) && snakes[p].head.x < (Math.floor(OBSTACLES[i].obstacle[j].position.x / CELL_SIZE) + Math.ceil(OBSTACLES[i].obstacle[j].position.width / WIDTH))) {
-                        isCollide = true;
-                    }
-                }
-            }
-        }
-    }
-
     if (isCollide) {
-        if (snake1.life === 1) {
-            gameOver.play()
-            alert("Game over");
-            snake1 = initSnake("purple");
-            drawLevel(snake1, "levelBoard");
-        } else {
-            snake1.life--;
-            snake1 = recentSnake(snake1);
-        }
+        setTimeout(() => {
+            alert("Game Over")
+        }, 150)
+        snake1 = initSnake()
     }
     return isCollide
 }
